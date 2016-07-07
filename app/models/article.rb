0dd0bd1d8841bceed76fa7d1fd956ae928fc67bc
Sergio <immawinnerbaby92@gmail.com>
2016-07-07 17:08:47 +0200

    first push

---
+++ app/models/article.rb
@@ -0,0 +1,6 @@
+class Article < ActiveRecord::Base
+	belongs_to :user
+	validates :title, presence:true, uniqueness: true
+	validates :body, presence:true, length: { minimum: 20}
+	#validates :name, format:{ with: /Expresion Regular/ }
+end
