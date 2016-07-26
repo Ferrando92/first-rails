fcde20c197f70178cccbb61975170c0b57dfd33a
Sergio <immawinnerbaby92@gmail.com>
2016-07-26 17:59:45 +0200

    Added and setted visit_counts + scaffold Comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,13 @@
 class ArticlesController < ApplicationController
-
+ before_action :authenticate_user!, except: [:show, :index]
+ before_action :set_article, only: [:destroy, :show]
+ 
  def index
    @articles = Article.all
  end
 
  def show
- 	@article = Article.find(params[:id])
+ 	@article.update_visits_count
  end
 
  def new
@@ -23,9 +25,12 @@ class ArticlesController < ApplicationController
  end
 
  def destroy
- 	@article = Article.find(params[:id])
- 	@article.destroy
+  	@article.destroy
  	redirect_to articles_path
+ end
+
+ def set_article	
+ 	@article = Article.find(params[:id])
  end
 
  def update
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,15 @@ class Article < ActiveRecord::Base
 	validates :title, presence:true, uniqueness: true
 	validates :body, presence:true, length: { minimum: 20}
 	#validates :name, format:{ with: /Expresion Regular/ }
+	before_create :set_visits_count
+
+	def set_visits_count
+		self.visits_count = 0
+	end
+	def update_visits_count
+		if self.visits_count.nil? 
+			self.set_visits_count
+		end
+		self.update(visits_count: self.visits_count + 1)
+	end
 end
